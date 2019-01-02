84d988ceda3955859a93c6d914f74eb7fcb9734d
Barbara Choy <barbchoy@gmail.com>
2019-01-02 22:22:18 +0000

    Done.

--- db/migrate/20181220190611_create_books.rb
+++ db/migrate/20181220190611_create_books.rb
@@ -2,6 +2,8 @@ class CreateBooks < ActiveRecord::Migration
   def change
     create_table :books do |t|
       t.string :title
+      t.integer :year_published
+      t.text :summary
       t.integer :author_id
       t.integer :category_id
     end
---
+++ db/seeds.rb
@@ -0,0 +1,29 @@
+books_list = {
+  "Beginning Ruby : From Novice to Professional" => {
+    :year_published => 2016,
+    :summary => "Your must-read guide to learning Ruby from the ground up, this book will teach you the principles behind object-oriented programming and within a few chapters you will be able to create a fully functional Ruby application. -- Edited summary fro"
+  }
+}
+
+categorys_list = {
+  "Computter Science" => {
+  },
+  "Philosophy and Psychology" => {
+  },
+  "Religion" => {
+  },
+  "Social Science" => {
+  },
+  "Language" => {
+  },
+  "Science" => {
+  },
+  "Technology and Applied Science" => {
+  },
+  "Arts and Recreation" => {
+  },
+  "Literature" => {
+  },
+  "History and Geography" => {
+  }
+}
