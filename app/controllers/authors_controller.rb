f5a154af9c110cd628c9c7e3cfb92dc8f7678b42
Barbara Choy <barbchoy@gmail.com>
2019-01-06 19:33:19 +0000

    Done.

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -7,16 +7,27 @@ class AuthorsController < ApplicationController
     set :session_secret, "secret_password"
   end
 
+  get '/authors/index' do
+    if !logged_in?
+      redirect to '/login'
+    else
+      @author = Author.find_by_slug(params[:slug])
+      binding.pry
+      erb :'authors/index'
+    end
+  end
+
   get '/authors/:slug' do
     if !logged_in?
       redirect to '/login'
     else
       @author = Author.find_by_slug(params[:slug])
+      binding.pry
       @books = @author.books
       erb :'authors/show'
     end
-
-
   end
+
+
 
 end
